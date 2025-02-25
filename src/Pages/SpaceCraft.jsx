6ba1f02d598810ddb5dd6fac8fd26fa93d7abbb7
THEJALAKSHMIjs <thejalakshmi622@gmail.com>
2025-02-25 23:27:20 +0530

    space

--- src/Pages/SpaceCraft.jsx
+++ src/Pages/SpaceCraft.jsx
@@ -6,19 +6,25 @@ function SpaceCraft() {
   return (
     <>
       <div className='bgimg'>
-       <Header/>
+        <Header />
         <div className='container' id='visit'>
           <div className="row">
-            
-            <div className="col-md-5">
-              <h1 className='spacecraft' style={{color: "#F7931A",
-              }}>VIST OUR <br /> SPACECRAFT</h1>
+            {/* Left Column: Spacecraft Heading */}
+            <div className="col-12 col-md-6 col-lg-5">
+              <h1 className='spacecraft' style={{ color: "#F7931A" }}>
+                VISIT OUR <br /> SPACECRAFT
+              </h1>
             </div>
-            <div className="col-md-2"></div>
-            <div className="col-md-5 ">
-              <div className='sentence1'>In the interest of cross-species understanding, we invite you to book a visit to our spacecraft, where you will experience stunning views, mind-expanding technology and exceptional Canis Minor hospitality (try the yurgenflark!). </div>
+
+            {/* Spacer Column (Hidden on Small Screens) */}
+            <div className="col-md-1 col-lg-2 d-none d-md-block"></div>
+
+            {/* Right Column: Description */}
+            <div className="col-12 col-md-5 col-lg-5">
+              <div className='sentence1'>
+                In the interest of cross-species understanding, we invite you to book a visit to our spacecraft, where you will experience stunning views, mind-expanding technology, and exceptional Canis Minor hospitality (try the yurgenflark!).
+              </div>
             </div>
-           
           </div>
         </div>
       </div>
