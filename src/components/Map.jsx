c92a3a0234c222265b1b39f2ab4f1f0a3c5fa25c
akashcp22 <akashcp22@gmail.com>
2025-03-14 17:55:18 +0530

    map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { MapContainer, TileLayer, Marker, Popup ,Tooltip} from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, Tooltip } from "react-leaflet";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import osm from "../api/osmProviders"
@@ -9,7 +10,7 @@ import icon1 from "../assets/icon1.png";
 import icon2 from "../assets/icon2.png";
 import icon3 from "../assets/icon3.png";
 import Footer from './Footer';
-import Header from "../components/Header"
+import Header from "../components/Header";
 
 // Store icons in an array
 const iconsArray = [icon1, icon2, icon3];
@@ -25,74 +26,71 @@ const getRandomIcon = () => {
   });
 };
 
-
-
 function Map() {
+  const [center, setCenter] = useState({ lat: 13.084622, lng: 80.248357 });
+  const [shops, setShops] = useState([]);
+  const ZOOM_LEVEL = 9;
+  const mapRef = useRef();
+  const navigate = useNavigate(); // Hook for navigation
 
-    const [center, setCenter] = useState({ lat: 13.084622, lng: 80.248357 });
-    const [shops, setShops] = useState([]);
-    const ZOOM_LEVEL = 9;
-    const mapRef = useRef();
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/products")
+      .then((response) => {
+        const products = response.data.products;
 
-    
-  
-    useEffect(() => {
-      axios
-        .get("http://localhost:4000/products")
-        .then((response) => {
-          const products = response.data.products;
-    
-          // Extract shop locations and attach productName to each shop
-          const shopLocations = products.flatMap((product) =>
-            product.availableShops.map((shop) => ({
-              ...shop,
-              productName: product.name, // Attach product name to each shop
-            }))
-          );
-    
-          setShops(shopLocations);
-    
-          // Center map on the first shop location (if available)
-          if (shopLocations.length > 0) {
-            setCenter({ lat: shopLocations[0].latitude, lng: shopLocations[0].longitude });
-          }
-        })
-        .catch((error) => console.error("Error fetching products:", error));
-    }, []);
-    
-  
+        // Extract shop locations and attach productName and productId to each shop
+        const shopLocations = products.flatMap((product) =>
+          product.availableShops.map((shop) => ({
+            ...shop,
+            productName: product.name, // Attach product name
+            productId: product._id, // Attach product ID
+          }))
+        );
+
+        setShops(shopLocations);
+
+        // Center map on the first shop location (if available)
+        if (shopLocations.length > 0) {
+          setCenter({ lat: shopLocations[0].latitude, lng: shopLocations[0].longitude });
+        }
+      })
+      .catch((error) => console.error("Error fetching products:", error));
+  }, []);
 
   return (
-<div className='mainbg' style={{backgroundColor:'#081f34'}}>
-  <Header/>
+    <div className='mainbg' style={{ backgroundColor: '#081f34' }}>
+      <Header />
       <div className='p-5'>
-        
-  <MapContainer center={center} zoom={ZOOM_LEVEL} ref={mapRef} style={{ height: "450px", width: "100%" }}>
-    <TileLayer url={osm.maptiler.url} attribution={osm.maptiler.attribution} />
-    
-    {/* Random Icons for Shop Markers */}
-    {shops.map((shop, index) => (
-  <Marker key={index} position={[shop.latitude, shop.longitude]} icon={getRandomIcon()}>
-  <Tooltip direction="top" offset={[0, -10]} opacity={1} permanent={false}>
-  <b>{shop.productName}</b> {/* Product name on hover */}
-  </Tooltip>
-  <Popup>
-  <b>{shop.productName}</b> <br /> 
-  {shop.shopName} <br />
-  {shop.address}
-  </Popup>
-  </Marker>
-  
-  ))}
-  
-  </MapContainer>
-  
-  
-  </div>
-  <div className='footback'><Footer/></div>
-</div>
-    
-  )
+        <MapContainer center={center} zoom={ZOOM_LEVEL} ref={mapRef} style={{ height: "450px", width: "100%" }}>
+          <TileLayer url={osm.maptiler.url} attribution={osm.maptiler.attribution} />
+
+          {/* Random Icons for Shop Markers */}
+          {shops.map((shop, index) => (
+            <Marker key={index} position={[shop.latitude, shop.longitude]} icon={getRandomIcon()}>
+              <Tooltip direction="top" offset={[0, -10]} opacity={1} permanent={false}>
+                <b>{shop.productName}</b> {/* Product name on hover */}
+              </Tooltip>
+              <Popup>
+                <b>{shop.productName}</b> <br />
+                {shop.shopName} <br />
+                {shop.address} <br />
+                {/* Clickable Button */}
+                <button 
+                  onClick={() => navigate(`/bodily-covering/${shop.productId}`)} 
+                  style={{ background: "blue", color: "white", padding: "5px 10px", border: "none", cursor: "pointer", marginTop: "5px" }}
+                >
+                  View Details
+                </button>
+              </Popup>
+            </Marker>
+          ))}
+
+        </MapContainer>
+      </div>
+      <div className='footback'><Footer /></div>
+    </div>
+  );
 }
 
-export default Map
+export default Map;
