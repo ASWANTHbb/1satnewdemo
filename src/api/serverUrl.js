de9a368545e3f4d14cdaf4cbe1d5e1769865ff85
bitmonkLab3x10 <9x3whale@gmail.com>
2025-04-11 13:48:46 +0530

    Update serverUrl.js
    server

--- src/api/serverUrl.js
+++ src/api/serverUrl.js
@@ -1,3 +1,3 @@
 // api/serverUrl.js
-  export const SERVER_URL = "https://onesat-db1.onrender.com"; 
+  export const SERVER_URL = "https://onesat-db-11-04-25.onrender.com"; 
 // export const SERVER_URL = "http://localhost:4000"; 
