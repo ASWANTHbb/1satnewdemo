5d527f1a8e907ac39b6b6c3a32640dd5e469b419
akashcp22 <akashcp22@gmail.com>
2025-04-12 15:15:38 +0530

    map

--- src/api/osmProviders.js
+++ src/api/osmProviders.js
@@ -1,6 +1,6 @@
 export default{
     maptiler: {
-        url:"https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=RFdWgj4bRKReQz8o4jKX",
+        url:"https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=W1BY3UoqFY8Nxue9utGO",
         attribution: '&copy; <a href="https://www.maptiler.com/copyright/" target="_blank">MapTiler</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 },
 }
